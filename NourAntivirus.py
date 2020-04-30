0e74a136f8d79a65b0fe6543a1c9d6253526df9d
linuxpussy <63728887+linuxpussy@users.noreply.github.com>
2020-04-30 18:01:17 +0200

    Add files via upload
    An other antivirus

---
+++ NourAntivirus.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -#
+
+class Antivirus():
+
+      print "Scanning"
+      print "Scan Finished! 3 trojans detected:"
+      print "Colox.Toxic.Adroj"
+      print "Coloxo.Toxice.bytes"
+      print "Aggro.Sapphire.STROJAN"
+      print "Colox.Toxic.Adroj !"
+      print "Colox.Toxice.bytes !"
+      print "Aggro.Sapphire.STROJAN !"
+      o = "Firewall is Active"
+      d = "Firewall need update"
+      t = "Firewall is Disabled."
+      o
+      print "000H68UH8U89IJU ! This is a virus defention"
+      print "CMOBONONOYHUFDXR675-7888888okokojpokpk ! This is a anonymate virus defention"
+      print "Rigids"
+      print "DDDDDDDDDDDDDDDDDDDDDDDDDDDDFGHNJ?.KLM%KHGTDRSEWXCDFGHNJ?KL virus defention!"
+      print "Updating...."
+      print "Finished."
+      print "System Secure!"
+   
+      print "With the Virusik Defends Free World License it's free!"        
+      print "Viruses are unable to counter NourAntivirus! Linux is secure with Nour Antivirus"
+
